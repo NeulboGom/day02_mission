f93a85162eb68c112fc7f0f9bb33f8b856a6f31d
NeulboGom <jinhee930409@inha.edu>
2023-01-13 17:06:25 +0900

    day02_mission Excercise 4.1 modified again

--- day02_mission.py
+++ day02_mission.py
@@ -2,7 +2,9 @@
 
 #4.1
 import random
-secret_number=random.randint(1,10)
+a=1
+b=10
+secret_number=random.randint(a,b)
 count=0
 
 while True:
@@ -11,8 +13,12 @@ while True:
     if secret_number == guess_number:
         print("Just Right!")
         break
-    elif secret_number > guess_number:
+    if guess_number not in range(a,b):
+        print("범위 밖의 숫자입니다. 다시 입력하세요.")
+    if secret_number > guess_number:
+        b=guess_number-1
         print("Too low")
     elif secret_number < guess_number:
+        a=guess_number+1
         print("Too high")
-print(f"{count}번만에 맞추셨습니다")
+print(f"{count}번만에 맞추셨습니다. 프로그램을 종료합니다.")
